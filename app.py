47bea79c626bca657a0043a84cbd655cac189880
mithunkumarg03 <mithung2311@gmail.com>
2026-04-27 00:04:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def chat():
                 "Content-Type": "application/json"
             },
             json={
-                "model": "llama3-8b-8192",
+                "model": "llama-3.3-70b-versatile",
                 "messages": [
                     {"role": "user", "content": message}
                 ]
